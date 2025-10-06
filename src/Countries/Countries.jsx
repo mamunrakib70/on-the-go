f38425b8dc397b7dbde0f47802069c70655bb1d5
mamunrakib70 <mamunrakib70@gmail.com>
2025-10-06 23:31:56 +0600

    country ol added

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -1,20 +1,36 @@
-import React, { use } from "react";
-import "./Countries.css"
+import React, { use, useState } from "react";
+import "./Countries.css";
 import Country from "../Country/Country.jsx";
 
-
 const Countries = ({ fetchCountries }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitedCountries = (country) => {
+    const newVisitedCountires = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountires);
+  };
+
   const countriesData = use(fetchCountries);
-  const countries=countriesData.countries;
+  const countries = countriesData.countries;
 
   return (
     <div>
       <h1 className="countries">Visited Countries: {countries.length}</h1>
-      <div className='country '>
-        {
-        countries.map(country=> <Country country={country}></Country>)
+      <h1 className="country-border">Total visited countries: {visitedCountries.length}</h1>
 
-      }
+      <ol>
+        {
+            visitedCountries.map((country)=>(<li key={country.cca3.cca3}>{country.name.common}</li>))
+        }
+      </ol>
+      <div className="country ">
+        {countries.map((country) => (
+          <Country 
+            key={country.cca3.cca3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+          ></Country>
+        ))}
       </div>
     </div>
   );
